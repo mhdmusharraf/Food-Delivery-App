0405e7e18629901c6d439aedc99320df1ff89815
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-08-08 16:53:36 +0530

    list all and remove food item functionality created

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -22,4 +22,30 @@ const addFood = async (req, res) => {
   }
 };
 
-export { addFood };
+// all food list
+
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// remove food item
+
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {}); // delete the image from the folder
+    await foodModel.findByIdAndDelete(req.body.id); // food data will be deleted from the database
+    res.json({ success: true, message: "Food Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood, listFood, removeFood };
