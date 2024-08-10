0f19a0df5d337039229670e19cc3f3433d948c7c
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-08-10 23:46:15 +0530

    user register api is successfully working

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRouter.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 // app config
 const app = express();
@@ -16,7 +18,8 @@ connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter);
-app.use("/images",express.static('uploads'));
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
