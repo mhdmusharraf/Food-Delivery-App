e0c7369e31e3b9f356216bfeb5e1f1f4e1ddd5ab
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-08-12 15:30:53 +0530

    user authentication features completed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,6 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000";
+  const [token,setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -18,17 +20,23 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  const getTotalCartAmount = () =>{
+  const getTotalCartAmount = () => {
     let totalAmount = 0;
-    for(const item in cartItems){
-      if(cartItems[item]>0){
-        let itemInfo = food_list.find((product)=> product._id===item);
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
         totalAmount += itemInfo.price * cartItems[item];
       }
     }
     return totalAmount;
-  }
- 
+  };
+
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      setToken(localStorage.getItem("token"));
+    }
+  },[])
+
   const contextValue = {
     food_list,
     cartItems,
@@ -36,6 +44,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken
   };
   return (
     <StoreContext.Provider value={contextValue}>
