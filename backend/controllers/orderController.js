13b23124a5965af27c79c71fc9fb739c1367969a
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-08-21 19:25:16 +0530

    Display order page in admin panel done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -74,14 +74,24 @@ const verifyOrder = async (req, res) => {
 
 // user orders for frontend
 const userOrders = async (req, res) => {
-  try{
-    const orders = await orderModel.find({userId:req.body.userId});
-    res.json({success:true,data:orders});
-  }catch(error){
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
     console.log(error);
-    res.json({success:false,message:"Error"});
-
+    res.json({ success: false, message: "Error" });
   }
 };
 
-export { placeOrder, verifyOrder, userOrders };
+// Listing orders for admin panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
